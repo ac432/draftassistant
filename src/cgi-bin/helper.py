826930e538dbe047cbfb9b4120f5cab8b0042190
Aaron Chu <ac3270@gmail.com>
2014-08-22 02:37:52 -0700

    draft table and player list

--- src/cgi-bin/helper.py
+++ src/cgi-bin/helper.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python
 import os
+import cgi
 import json
-import urlparse
 
-def something(param):
+def initialize(num_teams, draft_pos):
     with open("%s%s..%s..%sdata%splayers.json" % (os.path.dirname(os.path.realpath(__file__)), os.sep, os.sep, os.sep, os.sep)) as f:
         players = json.load(f)
-    output = {"cwd": os.getcwd(), "filedir": os.path.dirname(os.path.realpath(__file__)), "param": param, "players": players}
-    return output
+    teams = [{"name": "Your Team" if  draft_pos - 1 == i else "Team %s" % (i + 1), "players": []} for i in range(num_teams)]
+    draft_state = {"teams": teams, "remaining_players": players, "curr_round": 0, "curr_team": 0}
+    return draft_state
 
-args = urlparse.parse_qs(os.getenv("QUERY_STRING") or "")
+def select_player(draft_state, player_id):
+	draft_state["teams"][draft_state["curr_team"]]["players"].append(draft_state["remaining_players"][player_id]["name"])
+	return draft_state
 
-output = something(args["param"][0])
+form = cgi.FieldStorage()
+
+if "num_teams" in form and "draft_pos" in form:
+	draft_state = initialize(int(form.getvalue("num_teams")), int(form.getvalue("draft_pos")))
+elif "draft_state" in form and "player_id" in form:
+	draft_state = select_player(json.loads(form.getvalue("draft_state")), int(form.getvalue("player_id")))
 
 print "Content-type: application/json"
 print
-print json.dumps(output)
+print json.dumps(draft_state)
