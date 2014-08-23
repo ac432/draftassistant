9a1e04a45ca3551b505f6d84a029ab47ae99ce48
Aaron Chu <ac3270@gmail.com>
2014-08-23 13:08:25 -0700

    break out draft state obj into separate vars

--- src/cgi-bin/helper.py
+++ src/cgi-bin/helper.py
@@ -7,12 +7,12 @@ def initialize(num_teams, draft_pos):
     with open("%s%s..%s..%sdata%splayers.json" % (os.path.dirname(os.path.realpath(__file__)), os.sep, os.sep, os.sep, os.sep)) as f:
         players = json.load(f)
     teams = [{"name": "Your Team" if  draft_pos - 1 == i else "Team %s" % (i + 1), "players": []} for i in range(num_teams)]
-    draft_state = {"teams": teams, "players": players, "curr_round": 0, "curr_team": 0, "step": 1}
-    return draft_state
+    output = {"teams": teams, "players": players}
+    return output
 
 form = cgi.FieldStorage()
-draft_state = initialize(int(form.getvalue("num_teams")), int(form.getvalue("draft_pos")))
+output = initialize(int(form.getvalue("num_teams")), int(form.getvalue("draft_pos")))
 
 print "Content-type: application/json"
 print
-print json.dumps(draft_state)
+print json.dumps(output)
